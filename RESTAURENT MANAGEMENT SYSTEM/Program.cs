de1cdc87e424e0b11972ba9c0faa1511894afb9e
Nahid-Hossain-Dipu <mddipu4816@gmail.com>
2025-10-08 00:04:31 -0700

    Nothing

--- RESTAURENT MANAGEMENT SYSTEM/Program.cs
+++ RESTAURENT MANAGEMENT SYSTEM/Program.cs
@@ -17,7 +17,7 @@ namespace RESTAURENT_MANAGEMENT_SYSTEM
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            //Application.Run(new SalesReport());
+            //Application.Run(new SalesRegigit port());
             // Application.Run(new ReservationForm());
         }
     }
