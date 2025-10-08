3166d19bc8f14652a8ebef50805cbec6cb2c53c1
Nahid-Hossain-Dipu <mddipu4816@gmail.com>
2025-10-08 06:27:12 -0700

    Removed Commeents

--- RESTAURENT MANAGEMENT SYSTEM/Program.cs
+++ RESTAURENT MANAGEMENT SYSTEM/Program.cs
@@ -17,8 +17,6 @@ namespace RESTAURENT_MANAGEMENT_SYSTEM
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            //Application.Run(new SalesRegigit port());
-            // Application.Run(new ReservationForm());
         }
     }
 }
